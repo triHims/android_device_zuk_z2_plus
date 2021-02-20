0d7ed5a54d6679b73f07710e12cb9b4b8fbd6553
trihims <himanshu.16bcs1156@abes.ac.in>
2021-02-20 23:56:01 +0530

    Nuked overlay-lineage, this reverts  I00a45d.

--- device.mk
+++ device.mk
@@ -33,8 +33,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
